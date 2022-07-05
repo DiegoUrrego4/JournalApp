8bab1e8b8d05402fa4485084fbf445ec8dc8ddf8
Diego Urrego <diegourrg@gmail.com>
2022-07-05 14:05:53 -0500

    Primer commit de la aplicación. Avance en diseño de la aplicación y rutas.

---
+++ src/auth/pages/RegisterPage.jsx
@@ -0,0 +1,55 @@
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import { AuthLayout } from '../layout/AuthLayout';
+
+export const RegisterPage = () => {
+  return (
+    <AuthLayout title='Crear Cuenta'>
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Nombre Completo'
+              type='text'
+              placeholder='Tu nombre completo'
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Correo'
+              type='email'
+              placeholder='example@mail.com'
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Contraseña'
+              type='password'
+              placeholder='**********'
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant='contained' fullWidth>
+                Crear una cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
+            <Link component={RouterLink} color='inherit' to='/auth/login'>
+              Inicia sesión
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      <Typography>
+        Commodo fugiat do ex minim eu proident voluptate labore Lorem mollit ad
+        duis.
+      </Typography>
+    </JournalLayout>
+  );
+};
